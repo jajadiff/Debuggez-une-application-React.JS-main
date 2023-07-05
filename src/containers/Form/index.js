ede53cc535b40a69d2a2299da979a04a23fd2456
Jordan <jordanfurneri57@gmail.com>
2023-07-05 17:56:00 +0200

    Add onSucces dans le try

--- src/containers/Form/index.js
+++ src/containers/Form/index.js
@@ -4,7 +4,10 @@ import Field, { FIELD_TYPES } from "../../components/Field";
 import Select from "../../components/Select";
 import Button, { BUTTON_TYPES } from "../../components/Button";
 
-const mockContactApi = () => new Promise((resolve) => { setTimeout(resolve, 1000); })
+const mockContactApi = () =>
+  new Promise((resolve) => {
+    setTimeout(resolve, 1000);
+  });
 
 const Form = ({ onSuccess, onError }) => {
   const [sending, setSending] = useState(false);
@@ -16,6 +19,7 @@ const Form = ({ onSuccess, onError }) => {
       try {
         await mockContactApi();
         setSending(false);
+        onSuccess();
       } catch (err) {
         setSending(false);
         onError(err);
@@ -23,6 +27,8 @@ const Form = ({ onSuccess, onError }) => {
     },
     [onSuccess, onError]
   );
+  // console.log(onSuccess); // <--- Renvoie true
+  // console.log(onError) // <-- Renvoie null
   return (
     <form onSubmit={sendContact}>
       <div className="row">
@@ -56,11 +62,11 @@ const Form = ({ onSuccess, onError }) => {
 Form.propTypes = {
   onError: PropTypes.func,
   onSuccess: PropTypes.func,
-}
+};
 
 Form.defaultProps = {
   onError: () => null,
   onSuccess: () => null,
-}
+};
 
 export default Form;
