77a8b23966840ac34c4cecf74fce0000af8122ef
Jordan <jordanfurneri57@gmail.com>
2023-07-17 13:27:35 +0200

    Fix error filter select

--- src/components/Select/index.js
+++ src/components/Select/index.js
@@ -18,7 +18,7 @@ const Select = ({
   const changeValue = (newValue) => {
     onChange(newValue);
     setValue(newValue);
-    setCollapsed(newValue);
+    setCollapsed(true);
     // console.log(newValue);
     // console.log(onChange())
   };
